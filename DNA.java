6ebbd742a20c6e41d6033aa6605b5810cf890eff
Siphrn <lucaskordzakhia@yahoo.com>
2026-03-14 21:07:12 -0400

    Added the damn Code

---
+++ DNA.java
@@ -0,0 +1,18 @@
+public class DNA {
+  public static void main(String[] args) {
+
+    // Example DNA Seqence.
+    String dna = "ATGCGATACGCTTGA";
+
+
+    // Determines whether the DNA Strand contains a protein.
+    if (dna.indexOf("ATG") != -1 && dna.indexOf("TGA") != -1 && dna.length() % 3 == 0) {
+      int ATGIndex = dna.indexOf("ATG");
+      int TGAIndex = dna.indexOf("TGA");
+      System.out.println(dna.substring(ATGIndex, TGAIndex));
+      
+    } else {
+      System.out.println("No protein.");
+    }
+  }
+}
